87f683b74fe9668050300613c3cee25e67ca16e2
Victor Kozlov <punkrus18@gmail.com>
2018-03-24 10:51:20 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module'
+  },
+  extends: 'standard',
+  'rules': {
+    'arrow-parens': 0,
+    'generator-star-spacing': 0
+  }
+}
